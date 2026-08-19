fe3e34916b7598deaabc3f21e8cd38bab539877c
Ibrahim Hassan <hemahassantheking@gmail.com>
2026-08-20 01:58:28 +0300

    Fix model download for Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,27 +1,18 @@
 import os
-
-# Vercel filesystem is read-only except /tmp
-os.environ["HOME"] = "/tmp"
-os.environ["HF_HOME"] = "/tmp/huggingface"
-os.environ["HF_HUB_CACHE"] = "/tmp/huggingface/hub"
-os.environ["HF_XET_CACHE"] = "/tmp/huggingface/xet"
+import urllib.request
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .schemas import PredictionRequest, PredictionResponse
-from huggingface_hub import hf_hub_download
 import joblib
 import pandas as pd
+
 
 app = FastAPI(
     title="House Price Prediction API",
     version="1.0.0"
 )
 
-
-# =========================
-# CORS
-# =========================
 
 app.add_middleware(
     CORSMiddleware,
@@ -32,17 +23,17 @@ app.add_middleware(
 )
 
 
-# =========================
-# Download trained model from Hugging Face
-# =========================
+MODEL_PATH = "/tmp/house_price.pkl"
 
-MODEL_PATH = hf_hub_download(
-    repo_id="IbrahimHassan2oo5/house-price-model",
-    filename="house_price.pkl",
-    cache_dir="/tmp/huggingface"
-)
+if not os.path.exists(MODEL_PATH):
+    model_url = (
+        "https://huggingface.co/"
+        "IbrahimHassan2oo5/house-price-model/"
+        "resolve/main/house_price.pkl"
+    )
 
-# Load trained model
+    urllib.request.urlretrieve(model_url, MODEL_PATH)
+
 model = joblib.load(MODEL_PATH)
 
 
